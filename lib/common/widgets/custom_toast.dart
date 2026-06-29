45239f48645ccdd64cf80d9a9bb1e96eaf742687
Frank-jpeg <Frank-jpeg@users.noreply.github.com>
2026-06-30 00:47:03 +0800

    fix: improve toast readability

--- lib/common/widgets/custom_toast.dart
+++ lib/common/widgets/custom_toast.dart
@@ -11,6 +11,7 @@ class CustomToast extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final double toastOpacity =
         setting.get(SettingBoxKey.defaultToastOp, defaultValue: 1.0) as double;
     return Container(
@@ -18,17 +19,14 @@ class CustomToast extends StatelessWidget {
           EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 30),
       padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 10),
       decoration: BoxDecoration(
-        color: Theme.of(context)
-            .colorScheme
-            .primaryContainer
-            .withOpacity(toastOpacity),
+        color: colorScheme.primaryContainer.withOpacity(toastOpacity),
         borderRadius: BorderRadius.circular(20),
       ),
       child: Text(
         msg,
         style: TextStyle(
           fontSize: 13,
-          color: Theme.of(context).colorScheme.primary,
+          color: colorScheme.onPrimaryContainer,
         ),
       ),
     );
